2b53b2f45f9a98ef94757a3a21dce80329eb56dc
kimhyoseon <83405426+hyoseon-sno@users.noreply.github.com>
2023-04-28 10:28:26 +0900

    Update views.py

--- sejong_restaurants_map/app/views.py
+++ sejong_restaurants_map/app/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render, redirect as r
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from .models import restaurant
+from django.forms.models import model_to_dict
 
 def main(request):
-    Res=restaurant.objects.all()
-    print(Res)
-    if request.method == 'POST':
-        category = request.POST.get('category')
-        User.object.get(category=category)
-
     return render(
         request,
         'mapmain.html',
-        {'restaurant':Res}
+        {}
     )
 
+def category_data(request):
+    category = request.GET.get('category')
+    res=restaurant.objects.filter(category=category)
+    result = []
+    for r in res:
+        r2 = model_to_dict(r)
+        result.append(r2)
+
+    return JsonResponse(result, safe=False)
